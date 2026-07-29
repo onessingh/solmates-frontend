8abb3fc590486e98bc649e249fedf865361472c2
onessingh <thakurshivanisingh2321@gmail.com>
2026-07-29 23:22:38 +0530

    chore: bump cache v114.4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /*
- * SOLMATES Service Worker (v114.3 - NUCLEAR REFRESH)
+ * SOLMATES Service Worker (v114.4 - NUCLEAR REFRESH)
  * Handles background push notifications and offline caching.
  */
 
