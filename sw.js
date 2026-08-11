09372c3d2d5070f16b3ebd2ee09dcaaa6201a0d3
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-12 00:23:59 +0530

    UI: Pushed Exam Prep AI hero subtext slightly down for mobile view; Busted html cache to v4.1 and sw cache to v48

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v47';
+const CACHE_NAME = 'solmates-cache-v48';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
