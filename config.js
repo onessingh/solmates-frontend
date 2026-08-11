b46b1da037723619e8835955b2624ab50bad4dc9
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-11 14:05:40 +0530

    Bump cache version to v10 and config to 118.4

--- config.js
+++ config.js
@@ -61,7 +61,7 @@
   window.safeStorage = window.localStorage;
   window.safeSessionStorage = window.sessionStorage;
 })();
-window.SOL_APP_VERSION = '118.2';
+window.SOL_APP_VERSION = '118.4';
 
 // Completely hide dark mode from printers
 window.addEventListener('beforeprint', function() {
--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v9';
+const CACHE_NAME = 'solmates-cache-v10';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
