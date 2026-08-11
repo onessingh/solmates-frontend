e5dbe97de6815268fc8f952ba4fd918dd4c702a4
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-11 17:57:39 +0530

    fix: Update Exam Prep AI UI

--- config.js
+++ config.js
@@ -61,7 +61,7 @@
   window.safeStorage = window.localStorage;
   window.safeSessionStorage = window.sessionStorage;
 })();
-window.SOL_APP_VERSION = '120.7';
+window.SOL_APP_VERSION = '120.8';
 
 // Completely hide dark mode from printers
 window.addEventListener('beforeprint', function() {
--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v32';
+const CACHE_NAME = 'solmates-cache-v33';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
