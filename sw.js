c5f95b8b4bea3523dcedab57c0979b30fa62fcaf
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-12 01:03:25 +0530

    Chore: Comprehensive global cache bust; updated all HTML asset queries to v6.0 and service worker to v60

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v55';
+const CACHE_NAME = 'solmates-cache-v60';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
