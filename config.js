bf63eac1aa35a026cc7646ad64c623e8fbe68731
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-11 17:10:22 +0530

    Bump cache version to v28 and config to 120.3

--- config.js
+++ config.js
@@ -61,7 +61,7 @@
   window.safeStorage = window.localStorage;
   window.safeSessionStorage = window.sessionStorage;
 })();
-window.SOL_APP_VERSION = '120.2';
+window.SOL_APP_VERSION = '120.3';
 
 // Completely hide dark mode from printers
 window.addEventListener('beforeprint', function() {
