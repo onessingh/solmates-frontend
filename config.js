69be4e9437cb5194f4cccfa1aa0595e4c62651c6
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-11 14:51:05 +0530

    Fix final ReferenceError on click options in assessments and apply string fallback JSON parsing to Planners

--- config.js
+++ config.js
@@ -61,7 +61,7 @@
   window.safeStorage = window.localStorage;
   window.safeSessionStorage = window.sessionStorage;
 })();
-window.SOL_APP_VERSION = '118.8';
+window.SOL_APP_VERSION = '119.0';
 
 // Completely hide dark mode from printers
 window.addEventListener('beforeprint', function() {
