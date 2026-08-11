b01570f12c49f120c7cf0eed05db4b93a16a2101
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-11 19:25:12 +0530

    chore: bump cache to v47

--- config.js
+++ config.js
@@ -61,7 +61,7 @@
   window.safeStorage = window.localStorage;
   window.safeSessionStorage = window.sessionStorage;
 })();
-window.SOL_APP_VERSION = '122.1';
+window.SOL_APP_VERSION = '122.2';
 
 // Completely hide dark mode from printers
 window.addEventListener('beforeprint', function() {
--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v46';
+const CACHE_NAME = 'solmates-cache-v47';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
