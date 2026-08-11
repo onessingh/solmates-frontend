774a5ec24192deaa1cb9aa4ca5ce200cd670b565
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-11 18:09:32 +0530

    feat: Seed comprehensive list of exams

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v34';
+const CACHE_NAME = 'solmates-cache-v35';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
