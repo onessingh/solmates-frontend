6d1d4415f005d49de609fcf2a0f28187e8638ea8
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-11 18:25:01 +0530

    fix: Update Exam Prep subjects fallback and UI

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v38';
+const CACHE_NAME = 'solmates-cache-v39';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
