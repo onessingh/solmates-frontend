02dda6468d529f15ec5a96637b2cbc104a7c106f
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-11 16:44:05 +0530

    Fix: remove Hinglish from 9 insight tools, fix double-plus syntax error, rewrite personality/interest final analysis to show keywords and Holland Code, shuffle answer options randomly

--- config.js
+++ config.js
@@ -61,7 +61,7 @@
   window.safeStorage = window.localStorage;
   window.safeSessionStorage = window.sessionStorage;
 })();
-window.SOL_APP_VERSION = '120.0';
+window.SOL_APP_VERSION = '120.1';
 
 // Completely hide dark mode from printers
 window.addEventListener('beforeprint', function() {
--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v24';
+const CACHE_NAME = 'solmates-cache-v25';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
