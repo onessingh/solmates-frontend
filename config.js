feedbcd9b9713558acd1a6878ffaa4d88410a27d
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-11 15:21:49 +0530

    Inject completely bulletproof loop-based JSON extractor in safeParseJSON to perfectly handle ANY conversational text before or after the JSON

--- config.js
+++ config.js
@@ -61,7 +61,7 @@
   window.safeStorage = window.localStorage;
   window.safeSessionStorage = window.sessionStorage;
 })();
-window.SOL_APP_VERSION = '119.4';
+window.SOL_APP_VERSION = '119.5';
 
 // Completely hide dark mode from printers
 window.addEventListener('beforeprint', function() {
--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v18';
+const CACHE_NAME = 'solmates-cache-v19';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
