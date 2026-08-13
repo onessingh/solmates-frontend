45ba90679db575f0b1ab96f05e15713f9ad83ca3
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-13 18:46:23 +0530

    chore: bump asset cache version to 72.0 across all html files and sw.js

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v71';
+const CACHE_NAME = 'solmates-cache-v72';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
