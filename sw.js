25b5f47618dddadd680ffc1c50ab661e26addc20
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-13 16:29:01 +0530

    ui: Removed yellow color from AI Knowledge icon and bumped cache to v62

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v61';
+const CACHE_NAME = 'solmates-cache-v62';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
