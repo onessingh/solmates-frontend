564bf73c96d18f284c3050783fd4fbde059c6116
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-13 17:33:49 +0530

    feat(ai): extract actual resource link embedded inside ai-knowledge snippet

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v68';
+const CACHE_NAME = 'solmates-cache-v69';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
