7a4faf3db38cb58b96e2ed8c57acc5a56d8176f8
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-15 00:11:44 +0530

    fix: remove debug console from ui and add dark mode icon color in folder page

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v77';
+const CACHE_NAME = 'solmates-cache-v78';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
