684bbecafeb87d098b3563ed5a30a94a5b03018e
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-14 16:04:33 +0530

    chore: bump cache version to 76.0

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v75';
+const CACHE_NAME = 'solmates-cache-v76';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
