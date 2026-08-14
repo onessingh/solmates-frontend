2068d10c8be500d0d8c9543ed15c69c4dfc42d42
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-14 15:55:05 +0530

    fix: dark mode modal styling for youtube-browse, folder-content and elearning-subjects

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v73';
+const CACHE_NAME = 'solmates-cache-v74';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
