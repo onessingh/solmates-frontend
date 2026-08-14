2be7813397572cfaea0a9c9e5f62c4c6482a2447
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-14 16:17:51 +0530

    style: improve text colors for dark mode in view, elearning, and youtube pages

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v76';
+const CACHE_NAME = 'solmates-cache-v77';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
