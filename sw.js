5254a90ef39d1512d06593092ab7f6d5dc37b2e4
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-19 20:41:24 +0530

    fix: bump cache to v91 to wipe out broken redirects

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v88';
+const CACHE_NAME = 'solmates-cache-v91';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
