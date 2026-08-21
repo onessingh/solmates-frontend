f9720d0211cf833759e698d97ee03cdd4dbd5f0e
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-21 18:55:02 +0530

    fix(frontend): add view less toggle button to linkedin themes grid and update cache

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v99';
+const CACHE_NAME = 'solmates-cache-v100';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
