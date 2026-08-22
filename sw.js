298d1b4894d0316f4d8a58e5539fb7e7c22ad97b
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-22 11:23:48 +0530

    chore: bump SW cache to v102 for new templates

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v101';
+const CACHE_NAME = 'solmates-cache-v102';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
