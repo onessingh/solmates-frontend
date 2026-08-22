3368f5a7417ca76902702a5d5145f426832a7e48
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-23 00:44:01 +0530

    fix(v14): premium templates min-width 700px + scrollable preview, proper layout on mobile, v161

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v160';
+const CACHE_NAME = 'solmates-cache-v161';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
@@ -170,6 +170,7 @@ self.addEventListener('pushsubscriptionchange', function(event) {
         })
     );
 });
+
 
 
 
