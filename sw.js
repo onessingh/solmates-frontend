9898b1d6bf17b9a6277d00b54df2f7fccf92e989
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-22 13:48:03 +0530

    fix: remove 75 fake auto-generated templates, fix JSON Flat header visibility, set Banking & Finance as default template

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v115';
+const CACHE_NAME = 'solmates-cache-v117';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
