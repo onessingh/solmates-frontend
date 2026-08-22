7ac5fd528906e3f6954dcf991fc96156ba15e03c
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-22 13:25:20 +0530

    feat: make template grid scrollable, reorder templates, and add stylish CSS document skeletons to previews

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v113';
+const CACHE_NAME = 'solmates-cache-v114';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
