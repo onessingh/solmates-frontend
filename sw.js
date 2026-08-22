81e234b26d4d251f7d0db638125f8c9af69b61a1
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-22 12:19:12 +0530

    fix: overzealous regex causing false AI refusals on valid excuses

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v107';
+const CACHE_NAME = 'solmates-cache-v108';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
