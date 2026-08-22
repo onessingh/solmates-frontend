e416a162b91ea248c1bd3c9b244d56b208c1e291
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-22 12:28:24 +0530

    fix: make AI option parsing 100% bulletproof against conversational intro text

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v108';
+const CACHE_NAME = 'solmates-cache-v109';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
