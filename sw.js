702f67dcb198c5eb7835ce4e0f16d24d8e5ef5e4
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-22 13:10:07 +0530

    fix: repair style.css encoding corruption that was preventing new templates from rendering

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v111';
+const CACHE_NAME = 'solmates-cache-v113';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
