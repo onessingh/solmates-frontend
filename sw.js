add394eeec3c3dcbfa98b5dcdfccc5e51231f2a4
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-22 12:05:22 +0530

    feat: implement few-shot prompting to guarantee 3 formatted options and bypass LLM moralizers

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v105';
+const CACHE_NAME = 'solmates-cache-v106';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
