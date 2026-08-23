c516ab64c0430bbffcf515e6a7110dcb86a3e7d9
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-23 22:05:05 +0530

    fix(v86): hide education table headers in teacher template when no education details exist - v233

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v232';
+const CACHE_NAME = 'solmates-cache-v233';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
@@ -170,6 +170,7 @@ self.addEventListener('pushsubscriptionchange', function(event) {
         })
     );
 });
+
 
 
 
