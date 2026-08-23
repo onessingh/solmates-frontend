4c48cacc88d47c2f4bbf52ac93680c0a18deb1b7
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-23 18:51:36 +0530

    fix(v56): move Software Engineer + Data Scientist to By Profession, remove deleted Dark Aesthetic card - v203

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v202';
+const CACHE_NAME = 'solmates-cache-v203';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
@@ -170,6 +170,7 @@ self.addEventListener('pushsubscriptionchange', function(event) {
         })
     );
 });
+
 
 
 
