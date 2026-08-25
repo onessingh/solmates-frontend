6c8add404429881814a9905afd6a904089947a1c
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-25 23:42:16 +0530

    chore: bump cache for exam-prep UI fixes

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v265';
+const CACHE_NAME = 'solmates-cache-v266';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
