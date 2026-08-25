f69b0ea4932a07a5ace7ec7d20da1a5234b72a4b
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-25 13:19:32 +0530

    feat: highlight unselected questions on career test submit and scroll to first missing

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v261';
+const CACHE_NAME = 'solmates-cache-v262';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
@@ -170,6 +170,7 @@ self.addEventListener('pushsubscriptionchange', function(event) {
         })
     );
 });
+
 
 
 
