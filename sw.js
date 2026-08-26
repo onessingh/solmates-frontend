4fac1682082cc7c6d8a8d9f20142a060c9574442
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-26 20:23:48 +0530

    fix(translate): update Pro Tip wording to be mobile/TWA friendly, removing right-click references

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v303';
+const CACHE_NAME = 'solmates-cache-v304';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
