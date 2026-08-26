706f8d2aa14d6f7c603ca0d8bc1350a0065bd5eb
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-26 21:50:24 +0530

    chore: bump cache version to v305 for fresh deployment

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v304';
+const CACHE_NAME = 'solmates-cache-v305';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
