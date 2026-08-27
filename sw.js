ac0ec55edd81799f05346deced3014d87b44d239
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-27 14:29:05 +0530

    chore: bump cache to v340

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v339';
+const CACHE_NAME = 'solmates-cache-v340';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
@@ -170,6 +170,7 @@ self.addEventListener('pushsubscriptionchange', function(event) {
         })
     );
 });
+
 
 
 
