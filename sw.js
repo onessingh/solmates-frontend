c2c5e1ec65c8f0bf68fae7111257cdf5e2536630
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-29 21:32:03 +0530

    fix: restore perfectly balanced HTML files, fixes blank screens (v358)

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v357';
+const CACHE_NAME = 'solmates-cache-v358';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
