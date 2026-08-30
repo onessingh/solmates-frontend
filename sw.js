35fb266c2062a3ba68918d20af76912486a08300
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-30 23:14:03 +0530

    chore: bump cache to v387 for presence architecture

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v386';
+const CACHE_NAME = 'solmates-cache-v387';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
