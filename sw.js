86e7cc731e1094e8eacd5aea060a8d9c194bac12
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-30 18:33:59 +0530

    chore: force cache refresh to guarantee guest PC gets the v377 deafness fix (v378)

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v377';
+const CACHE_NAME = 'solmates-cache-v378';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
