526c2bcb23edf0d59e7fc75b774f281764b259c0
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-30 12:01:02 +0530

    chore: bump cache to v362

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /*
- * SOLMATES Service Worker (v116.9 - NUCLEAR REFRESH)
+ * SOLMATES Service Worker (v117.0 - Cache Bump)
  * Handles background push notifications and offline caching.
  */
 
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v361';
+const CACHE_NAME = 'solmates-cache-v362';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
