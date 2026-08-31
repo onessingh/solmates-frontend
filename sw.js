a422cbcc88df186cc64aee84b93ff13a08f799eb
onessingh <thakurshivanisingh2321@gmail.com>
2026-08-31 11:22:20 +0530

    v396 - Cache bump

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v395';
+const CACHE_NAME = 'solmates-cache-v396';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
