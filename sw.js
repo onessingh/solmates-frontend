ec5c49be8772134be74e01cb1cb6e2cfff9bf2f3
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 16:30:18 +0530

    Refactor form submission to bypass event listener and HTML5 validation issues

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v427';
+const CACHE_NAME = 'solmates-cache-v428';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
