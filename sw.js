26d9626d4fbb3130622ea4df3cb625f0bf5a7447
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 17:17:55 +0530

    Update View More UI: Place image at the top of a distinct content box when expanded, hide image when clamped

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v435';
+const CACHE_NAME = 'solmates-cache-v436';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
