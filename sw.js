c8a3136cb394db2435db55f2004ad6ad55630974
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 18:49:14 +0530

    Rename View Full Post back to View More and add a View Less button to collapse the box back to its original state

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v447';
+const CACHE_NAME = 'solmates-cache-v448';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
