5388617b8fc632fff488f517aea3112395046f16
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 16:53:27 +0530

    Completely rewrite submitNoticeForm to prevent silent crashes and improve error visibility

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v430';
+const CACHE_NAME = 'solmates-cache-v431';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
