05f89b2527dd723f513e21dfd29556a68e026ae3
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 16:16:52 +0530

    Bump SW to v426

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v425';
+const CACHE_NAME = 'solmates-cache-v426';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
