71a6a98af268aa8b62949668c1241f68cc0d5ead
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 14:54:33 +0530

    Cache bust APK download and bump SW to v423

--- js/install-banner.js
+++ js/install-banner.js
@@ -68,7 +68,7 @@
         document.getElementById('solmates-install-btn').onclick = (e) => {
             e.stopPropagation();
             banner.remove();
-            window.location.href = '/solmates.apk';
+            window.location.href = '/solmates.apk?v=19';
         };
 
         // Hide when clicking outside
