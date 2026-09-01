30727904215ce3185af21f66424dcfd86c085170
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 11:58:35 +0530

    Remove prefersDark inline logic from games and pdf tools so they always load as light by default; bump SW v414

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v413';
+const CACHE_NAME = 'solmates-cache-v414';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
