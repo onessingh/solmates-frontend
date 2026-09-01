c0a8f3f3e6d14cab9bfec3cdeb8138f3cd8b7953
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 21:18:17 +0530

    Fix JavaScript ReferenceError (Cannot access 'imageUrl' before initialization) by moving the let declaration to the top of the scope so it can be safely appended to the link for rich posts.

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v449';
+const CACHE_NAME = 'solmates-cache-v450';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
