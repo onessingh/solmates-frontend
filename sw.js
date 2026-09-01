bc6bdd71ab05c8e4c0c99710b8f9c5ce0acf2673
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 18:43:49 +0530

    Fix Promise Error: Restore newBadge and isNew declarations that were accidentally swallowed by a greedy regex during the richpost link Html patch

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v446';
+const CACHE_NAME = 'solmates-cache-v447';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
