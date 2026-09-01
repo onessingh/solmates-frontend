6a15a47cf68dd056faeb1d1fe002538df08c2cec
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 17:11:49 +0530

    Fix UI bugs: Hide 'Coming Soon' when image/desc present, add 'View More' toggle for long text and images, and prevent long URLs from breaking card layout

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v434';
+const CACHE_NAME = 'solmates-cache-v435';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
