5e02c4348ecda1853246a8a3caaf1511f4dd19b0
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 23:13:53 +0530

    Force service worker update by unregistering old workers, bumping URL to v=92, and bumping cache to v463.

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v462';
+const CACHE_NAME = 'solmates-cache-v463';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
