cbe3530bcf294d9347f4eee04986475ed4593f4c
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 12:54:21 +0530

    Update quiz-battle questions per game options to 10, 20, 25; bump SW v418

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v417';
+const CACHE_NAME = 'solmates-cache-v418';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
