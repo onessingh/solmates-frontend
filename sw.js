c15ccb8af2c5de4d02934828644871c15c2b84e7
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 16:59:20 +0530

    Fix mobile scrolling issue where Add Notice button was hidden off screen; reduce form height

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v431';
+const CACHE_NAME = 'solmates-cache-v432';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
