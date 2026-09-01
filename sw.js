43a7603fecb53a12528769ffbd67b0adab94d00a
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 11:30:35 +0530

    Opt out of auto dark mode strictly using only light; bump sw cache to v410

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v409';
+const CACHE_NAME = 'solmates-cache-v410';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
