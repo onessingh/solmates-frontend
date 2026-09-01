bdeedf2b660b69c99496d6df3c8f433e4e9c3fa2
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 16:11:05 +0530

    Bump SW cache to v425

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v424';
+const CACHE_NAME = 'solmates-cache-v425';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
