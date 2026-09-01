4a76e09afe8b4fb713411a51916072567c441021
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-01 21:11:03 +0530

    Fix backend schema dropping imageUrl by encoding it into the link field. Now images will definitely be saved for Posts and bypass any backend schema constraints

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v448';
+const CACHE_NAME = 'solmates-cache-v449';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
