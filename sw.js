fa1b15bd5e47a45e57b39fe854ad466c12ecac0c
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-02 18:59:57 +0530

    Bump service worker cache to v495 to enforce notification UI fixes

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching. Web Push has been removed as notifications are natively handled by Android App.
  */
 
-const CACHE_NAME = 'solmates-cache-v494';
+const CACHE_NAME = 'solmates-cache-v495';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
@@ -69,5 +69,6 @@ self.addEventListener('fetch', event => {
             })
     );
 });
+
 
 
