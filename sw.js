f832ed447290729f9cdddf3c3f6cd3c739a82de7
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-02 12:57:43 +0530

    Restore FCM token extractor in index.html which was accidentally removed

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v468';
+const CACHE_NAME = 'solmates-cache-v469';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
