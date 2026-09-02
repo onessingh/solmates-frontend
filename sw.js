8b0141286f3fca700d50ea77188c91291e87d8fb
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-02 13:07:51 +0530

    Auto-subscribe new app users to all semesters and remove stay updated popup

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v469';
+const CACHE_NAME = 'solmates-cache-v470';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
