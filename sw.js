46189eb670df8bc9eb3ce546ec9403382a2d0af2
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-02 16:08:05 +0530

    Fix dark mode text colors in semester modal; Let modal image take full width

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('notificationclick', function(event) {
     );
 });
 
-const CACHE_NAME = 'solmates-cache-v479';
+const CACHE_NAME = 'solmates-cache-v480';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
