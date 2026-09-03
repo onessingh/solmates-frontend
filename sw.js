8dbf42f5da4b1acbea200db8b9a97175dcf4ac65
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-03 17:07:37 +0530

    v517: Revert to ALWAYS #0f172a theme-color because TWA intent is buggy across OS themes

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching. HTML pages are network-first, assets are cache-first.
  */
 
-const CACHE_NAME = 'solmates-cache-v516';
+const CACHE_NAME = 'solmates-cache-v517';
 
 const STATIC_ASSETS = [
     '/notification.html',
@@ -64,5 +64,6 @@ self.addEventListener("fetch", event => {
         })
     );
 });
+
 
 
