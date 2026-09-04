a97cd2cad08b49e76702ba2c3d8b7792fe7929fe
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-04 19:01:54 +0530

    Update: new TWA APK built with www.solmates.in deep-linking support & cache v547 bump

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching. HTML pages are network-first, assets are cache-first.
  */
 
-const CACHE_NAME = 'solmates-cache-v546';
+const CACHE_NAME = 'solmates-cache-v547';
 
 const STATIC_ASSETS = [
     '/notification.html',
