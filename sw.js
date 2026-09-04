32f7200ba93b8c09324af00d7d6b87aceb8128f4
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-04 19:41:11 +0530

    UI: Revert removal of back arrow & bump cache v549

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-/**
+﻿/**
  * SOLMATES Service Worker (v514)
  * Handles offline caching. HTML pages are network-first, assets are cache-first.
  */
 
-const CACHE_NAME = 'solmates-cache-v548';
+const CACHE_NAME = 'solmates-cache-v549';
 
 const STATIC_ASSETS = [
     '/notification.html',
