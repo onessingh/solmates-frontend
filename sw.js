9a66f9dc0a6d3aaa15f9e54dc7464344a5194808
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-06 21:19:27 +0530

    UI: Add skeleton shimmer loader for post images in notification page

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching. HTML pages are network-first, assets are cache-first.
  */
 
-const CACHE_NAME = 'solmates-cache-v600';
+const CACHE_NAME = 'solmates-cache-v601';
 
 const STATIC_ASSETS = [
     '/notification.html',
