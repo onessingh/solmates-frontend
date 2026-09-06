30e1a4154b22d6b78337deeeabc08b2a68560d07
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-06 21:45:58 +0530

    UI: Use switchMobileTab to correctly navigate to live preview side in portfolio builder

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching. HTML pages are network-first, assets are cache-first.
  */
 
-const CACHE_NAME = 'solmates-cache-v604';
+const CACHE_NAME = 'solmates-cache-v605';
 
 const STATIC_ASSETS = [
     '/notification.html',
