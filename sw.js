38dc4401e96ea9fc1c4b79505d07551a624320d9
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-07 21:25:26 +0530

    UI: Update back button on Healthcare page to point to Database

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching. HTML pages are network-first, assets are cache-first.
  */
 
-const CACHE_NAME = 'solmates-cache-v618';
+const CACHE_NAME = 'solmates-cache-v619';
 
 const STATIC_ASSETS = [
     '/notification.html',
