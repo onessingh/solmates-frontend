d3f015bac04387ac512fd599c368b51f8943cfe5
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-07 22:18:27 +0530

    Feat: Popup lock modal with dark mode, better lock icon, MBA section locks on database page

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching. HTML pages are network-first, assets are cache-first.
  */
 
-const CACHE_NAME = 'solmates-cache-v624';
+const CACHE_NAME = 'solmates-cache-v625';
 
 const STATIC_ASSETS = [
     '/notification.html',
