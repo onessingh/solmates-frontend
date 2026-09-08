be4380d6f4a28d210d6434911fe567a5b2dd4bc0
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-08 12:06:39 +0530

    Bump cache version to v646

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching. HTML pages are network-first, assets are cache-first.
  */
 
-const CACHE_NAME = 'solmates-cache-v645';
+const CACHE_NAME = 'solmates-cache-v646';
 
 const STATIC_ASSETS = [
     '/notification.html',
