cc775d241e53df009221574698c7739c8002b663
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-08 15:59:15 +0530

    Revert: Restore video-viewer.html and pdf-viewer.html to stable pre-zoom-hack version (3d047eaf) & bump cache to v654

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching. HTML pages are network-first, assets are cache-first.
  */
 
-const CACHE_NAME = 'solmates-cache-v653';
+const CACHE_NAME = 'solmates-cache-v654';
 
 const STATIC_ASSETS = [
     '/notification.html',
