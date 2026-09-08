b7a64deccde3936a66107fd65a4fac18b2c2a22c
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-08 14:46:44 +0530

    Force cache bump to v653 for manual refresh

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching. HTML pages are network-first, assets are cache-first.
  */
 
-const CACHE_NAME = 'solmates-cache-v652';
+const CACHE_NAME = 'solmates-cache-v653';
 
 const STATIC_ASSETS = [
     '/notification.html',
