374e5864d16b5e5f503983df5e93bc591eb0ffb1
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-14 13:27:46 +0530

    Bump cache to v677

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching. HTML pages are network-first, assets are cache-first.
  */
 
-const CACHE_NAME = 'solmates-cache-v676';
+const CACHE_NAME = 'solmates-cache-v677';
 
 const STATIC_ASSETS = [
     '/notification.html',
