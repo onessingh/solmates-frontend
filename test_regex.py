40a746e1af561c2f599b5dc4b45ccfe73f4a7c16
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-17 14:21:01 +0530

    Chore: Bump cache version to v712 and update sitemap lastmod date to today

--- test_regex.py
+++
@@ -1,8 +0,0 @@
-﻿import os
-import re
-base_dir = r'c:\Users\Toshiba\OneDrive\Desktop\solmates\frontend'
-index_path = os.path.join(base_dir, 'index.html')
-with open(index_path, 'r', encoding='utf-8') as f:
-    index_html = f.read()
-nav_center_match = re.search(r'<div class="nav-center">.*?</div>', index_html, re.DOTALL)
-print(nav_center_match.group(0))
