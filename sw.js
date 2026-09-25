911baa879ea07813103635c3fea6aca968080ea0
onessingh <thakurshivanisingh2321@gmail.com>
2026-09-25 18:27:52 +0530

    Chore: Bump cache version to v780 as requested

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /**
- * SOLMATES Service Worker (v779)
+ * SOLMATES Service Worker (v780)
  * Handles offline caching. HTML pages are network-first, assets are cache-first.
  */
 
-const CACHE_NAME = 'solmates-cache-v779';
+const CACHE_NAME = 'solmates-cache-v780';
 
 const STATIC_ASSETS = [
     '/notification.html',
